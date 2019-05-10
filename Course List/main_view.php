af4f22313f28c26a0e85c344b4ebda9eb44d2be9
Cappuccinomild <33108600+Cappuccinomild@users.noreply.github.com>
2019-05-10 15:35:33 +0900

    수강목록 출력

--- Course List/main_view.php
+++ Course List/main_view.php
@@ -7,30 +7,50 @@
   $base->link = './style.css';
 
   //$id = $_SESSION['id'];
-  $id = "2017112000";
+  $id = "2017000000";
 
   $db = new DBC;
 
   $db->DBI();
 
   //학생이 수강중인 수업번호 가져오기
-  $db->query = "SELECT Cname, Pnum, Course_room, Cstime, Cftime, Cday FROM	COURSE, STUDENT, TAKE_CLASS, LOCATION, CLASSROOM, CLASSHOUR WHERE	Snumber = '".$id."' AND Snumber = Snum AND Cno = Cnumber AND Cnumber = Conum AND Course_room = Class_room";
+  $db->query = "SELECT Cname, Pnum, Course_room, Cstime, Cftime, Cday FROM	COURSE, STUDENT, TAKE_CLASS, LOCATION, CLASSROOM, CLASSHOUR WHERE	Snum = '".$id."' AND Snumber = Snum AND Cno = Cnumber AND Cnumber = Conum AND Course_room = Class_room";
 
   $db->DBQ();
 
   $num = $db->result->num_rows;
 
-  $base->content .= "<h1>수강 목록</h1><br><div class = 'course_style'>";
-  $base->content .= " 강의명 번호 강의실 강의시간 ~ 강의 날짜 <br>";
+  //수업 목록 출력
+  $base->content .= "<h1>수강 목록</h1><br>";
+  $base->content .= "<table>";
+  $base->content .= "<tr style=' padding: 10px;' id=course_title>
+                      <th>Cname</th>
+                      <th>Pnum</th>
+                      <th>Course_room</th>
+                      <th>Cstime</th>
+                      <th>Cftime</th>
+                      <th>Cday</th>
+                    </tr>";
+
   for($i = 0 ; $i < $num ; $i ++){
 
     $data = $db->result->fetch_row();
 
-    $base->content .= "  '".$data[0]."' '".$data[1]."' '".$data[2]."' '".$data[3]."' '".$data[4]."' '".$data[5]."' <br>";
+    $base->content .= "<tr style='text-align: center; padding: 10px; ' id=course_text>
+                        <td>".$data[0]."</td>
+                        <td>".$data[1]."</td>
+                        <td>".$data[2]."</td>
+                        <td>".$data[3]."</td>
+                        <td>".$data[4]."</td>
+                        <td>".$data[5]."</td>
+                      </tr>";
 
   }
 
-  $base->content .= "</div>";
+  $base->content .= "</table>";
+
+  //시험 시간표 출력
+  $base->content .= "<h1>시험 시간</h1><br><div class = 'test_timetable'>";
 
   $base->LayoutMain();
 
