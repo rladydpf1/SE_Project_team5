50aba552ff40a193b17cf44f0b1ce1b132d696b6
Cappuccinomild <33108600+Cappuccinomild@users.noreply.github.com>
2019-06-19 04:01:43 +0900

    시간표출력

--- Exam Schedule/schedulei.php
+++ Exam Schedule/schedulei.php
@@ -23,14 +23,17 @@ else {
 
 $location_name = $_GET['location_name'];
 $classroom = $_GET['classroom'];
+$timetable_data = array();
+$day = ['MON' => 2, 'THU' => 3, 'WEN' => 4, 'TUR' => 5, 'FRI' => 6, 'SAT' => 7, 'SUN' => 1];
+/*
+$array = "new Array(1, 2, 3, 4, 5)";
+$base->script = "var arr = $array; var i; document.write($Cnumber); for(i = 0 ; i < arr.length ; i ++)document.write(arr[i]);";
+*/
+$base->content = "<iframe name = 'timetable' src = 'timetable_view.php?day=2&location_name=1&classroom=1+345' width = '100%' height = '200px'  frameborder=0 framespacing=0 marginheight=0 marginwidth=0 scrolling=no vspace=0 style = 'margin-left: auto; margin-right: auto; width = '100%''></iframe>
 
-$base->content = "";
-
-$base->content = "
-
-   <form action='./schedulei.php' method='GET'>
+   <form action='./timetable_view.php' target='timetable' method='GET'>
      <div>
-        <p> 시험 일정 선택 </p>
+        <p style = 'text-align : center;'> 시험 일정 선택 </p>
         <table style='width: 60%;background-color: #ffffff; margin-left: auto; margin-right: auto; border-radius: 5px; height: 500px; border-top: solid; border-bottom:solid;'>
            <tr>
 
@@ -41,6 +44,22 @@ $base->content = "
               </td>
 
            </tr>
+           <tr>
+
+              <td><label for='day' style='font-family: 휴먼모음T; font-size: 20px; color: #000000; float: left;'>요일</label></td>
+
+              <td>
+                <select name='day' id = 'day'>
+                  <option value = '1'>일</option>
+                  <option value = '2'>월</option>
+                  <option value = '3'>화</option>
+                  <option value = '4'>수</option>
+                  <option value = '5'>목</option>
+                  <option value = '6'>금</option>
+                  <option value = '7'>토</option>
+                </select>
+              </td>
+           </tr>
 
            <tr>
 
@@ -48,8 +67,6 @@ $base->content = "
 
               <td>
                 <select name='location_name' id = 'location'>";
-
-
 
 $db->query = "SELECT * FROM LOCATION";
 
@@ -80,6 +97,7 @@ $base->content .="</select>
               <td>
                 <select name='classroom' id = classroom>";
 
+
 $db->query = "SELECT * FROM CLASSROOM";
 
 $db->DBQ();
@@ -105,16 +123,95 @@ $base->content .="</select>
            </tr>
 
            <tr>
-
-              <td colspan='2'><input type='submit' value='예약하기' id='submit-btn'/></td>
-
+              <td><input type='submit' value='조회' id='submit-btn'/></form></td>
+              <td><form></form><input type='submit' value='예약하기' id='submit-btn'/></td>
            </tr>
           </table>
          </div>
-   </form>
+
 
 ";
 
 $base->LayoutMain();
+/*
+$i = 0;
+$db->query = "SELECT MAX(Lnumber) AS max_number FROM LOCATION";// -- for문을 돌릴 때 쓰는 값
 
+$db->DBQ();
+$MAX = $db->result->fetch_row();
+$MAX = $MAX[0];
+
+$buildings ='[';
+for($i = 1 ; $i <= $MAX; $i++){
+  $db->query = "SELECT Class_room FROM CLASSROOM WHERE Lnum = $i";
+  $db->DBQ();
+  if($db->result){
+    $buildings .= '[';
+
+    for($j = 0; $j < $db->result->num_rows; $j++){
+      $data = $db->result->fetch_row();
+      if($j != $db->result->num_rows - 1)
+        $buildings .= $data[0] .', ';
+      else
+        $buildings .= $data[0];
+    }
+    if($i != $MAX)
+      $buildings .= '],';
+    else
+      $buildings .= ']';
+  }
+}
+$buildings .= ']';
+
+echo $buildings;
+
+for($i = 1 ; $i <= $MAX ; $i++){
+  for($j = 0 ; $j < count($timetable_data[$i]); $j++){
+    $Class_room = $timetable_data[$i][$j][0];
+    $db->query = "SELECT Estime, Eftime, Eday, Cname FROM EXAM, COURSE WHERE Cnumber = Cnum AND Exam_room = '$Class_room'";
+    $db->DBQ();
+
+    if($db->result){
+      $k = 0;
+      while($data = $db->result->fetch_row()){
+        $data = str_replace(';'.$data[2], '',implode(';', $data));
+
+        echo $timetable_data[$i][$j][1][$day[$data[2]]][$k++] = $data;
+
+      }
+    }
+  }
+}
+$total_data ='[';
+for($i = 1; $i <= $MAX; $i++){
+  $total_data .='[';
+  for($j = 0; $j < count($timetable_data[$i]); $j++){
+    $total_data .='[[],';
+    for($k = 1; $k <= 7; $k++){
+      $total_data .='[';
+      for($p = 0; $p < count($timetable_data[$i][$j][1][$k]); $p++){
+        $total_data .= $timetable_data[$i][$j][1][$k][$p];
+        if($p != count($timetable_data[$i][$j][1][$k]) - 1)
+          $total_data .= ', ';
+
+      }
+      if($k != 7)
+        $total_data .= '],';
+      else
+        $total_data .= ']';
+    }
+    if($j != count($timetable_data[$i])-1)
+      $total_data .= '],';
+    else
+      $total_data .= ']';
+  }
+  if($i != $MAX)
+    $total_data .= '],';
+  else
+    $total_data .= ']';
+}
+$total_data .= ']';
+
+echo $total_data;
+*/
 ?>
---
+++ Exam Schedule/timetable_view.php
@@ -0,0 +1,87 @@
+<?php
+
+  require_once '../layout.inc';
+  require_once '../Database/db.php';
+
+  $base = new Layout;
+  $base->link = './timetable.css';
+
+  $db = new DBC;
+
+  $db->DBI();
+  $location_name = 1;
+  $date = 1;
+
+  $location_name = $_GET['location_name'];
+  $classroom = $_GET['classroom'];
+  $date = $_GET['day'];
+  $day = ['', 'SUN', 'MON', 'TUE', 'WEN', 'TUR', 'FRI', 'SAT'];
+  $classrooms;
+  $timetable;
+
+  $db->query = "SELECT Class_room FROM CLASSROOM WHERE Lnum = $location_name";
+  $db->DBQ();
+  $num = $db->result->num_rows;
+
+  for($j = 0 ; $data = $db->result->fetch_row(); $j++)
+    $classrooms[$j] = $data[0];
+
+  $base->content .= "<p>".$day[$date]."</p>";
+
+
+  $base->content .= "<table style='width:100%;background-color: #ffffff; margin-left: auto; margin-right: auto; border-radius: 5px;border-top: solid; border-bottom:solid;'><tr><td>강의실</td>";
+
+  for($i = 8 ; $i < 23; $i++){
+
+     $base->content .= "<td colspan = '12'>$i</td>";
+
+  }
+ $base->content .= "</tr>";
+
+ for($i = 0 ; $i < $num; $i++){
+   $timetable = array();
+   $size = 180;
+   $base->content.= "<tr><td>$classrooms[$i]</td>";
+   $db->query = "SELECT Cname, Exam_room, Estime, Eftime FROM COURSE, EXAM WHERE Cnumber=Cnum AND Eday = '$day[$date]' AND Exam_room = '$classrooms[$i]'";
+   $db->DBQ();
+
+
+   if($db->result)
+     while($data = $db->result->fetch_row()){
+
+       $Stime = explode(':', $data[2]);
+       $Etime = explode(':', $data[3]);
+
+       $Stime[0] = intval($Stime[0]);
+       $Stime[1] = intval($Stime[1]);
+       $Etime[0] = intval($Etime[0]);
+       $Etime[1] = intval($Etime[1]);
+
+       $j = intval(($Stime[0] * 60 + $Stime[1] - 480)/5);
+       $timetable[$j] = intval(($Etime[0] * 60 + $Etime[1] - 480)/5) - $j;
+
+     }
+   for($j = 0; $j < $size; $j ++){
+
+     if($timetable[$j]){
+
+       $base->content .= "<td colspan = '$timetable[$j]' style = 'background : gray;'> </td>";
+       $size -= $timetable[$j];
+       $size += 1;
+       for($k = 0; $k < $timetable[$j] ; $k ++) // 이 자리부터 시간표만큼 테이블을 만들지 않음
+         $timetable[$j + $k] = 1;
+     }
+
+     else if($timetable[$j] == 1){
+
+     }
+
+     else
+       $base->content .= "<td></td>";
+   }
+   $base->content .= "</tr><tr>";
+ }
+$base->content .= "</tr></table>";
+ $base->LayoutViewer();
+
+?>
