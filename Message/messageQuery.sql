44e3e88bcd9ab0b048095042377263f80c0234bb
rladydpf1 <45200209+rladydpf1@users.noreply.github.com>
2019-06-18 16:18:44 +0900

    수락/거절 후 자동으로 답장

--- Message/messageQuery.sql
+++ Message/messageQuery.sql
@@ -12,6 +12,24 @@ WHERE Receiver = '받는사람' AND RCnumber = Cnumber AND Course_room = Class_r
 
 -- messagedetail.php : 요청 메시지의 상세한 정보 및 본문 내용을 보여주는 페이지
 -- MESSAGE 테이블의 모든 정보를 꺼낸다.
-SELECT * FROM MESSAGE WHERE Mnumber = num;
+SELECT * FROM MESSAGE WHERE Mnumber = $num;
 
 -- messageaprocess.php : 요청 메시지를 수락/거절했을 경우 해당 부분을 처리하는 페이지
+-- 거절한 경우 : 거절했다고 자동으로 답장을 보낸다.
+SELECT Receiver, Sender, Title FROM MESSAGE WHERE Mnumber = $num;
+UPDATE MESSAGE 
+SET Receiver = '$Sender' AND Sender = '$Receiver' AND Title = '<거절> ' + '$title'
+WHERE Mnumber = $num;
+
+-- 수락한 경우 :
+SELECT MAX(Enumber) AS max_number FROM EXAM; -- 시험일정이 아무것도 없는 경우도 처리해야한다.
+SELECT SCnumber, Course_room, Mstime, Mftime, Mday
+FROM MESSAGE, COURSE
+WHERE Mnumber = $num AND Cnumber = RCnumber;
+INSERT INTO EXAM VALUES (max_number + 1, $SCnumber, '$Course_room', '$Mstime', '$Mftime', '$Mday');
+
+--수락했다고 자동으로 답장을 보낸다.
+SELECT Receiver, Sender, Title FROM MESSAGE WHERE Mnumber = $num;
+UPDATE MESSAGE 
+SET Receiver = '$Sender' AND Sender = '$Receiver' AND Title = '<수락> ' + '$title'
+WHERE Mnumber = $num;
