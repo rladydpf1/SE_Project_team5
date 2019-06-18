8d98c1ec65c2090cf0b2e9cbf486066bcfbb0bac
meaning2415 <33250675+meaning2415@users.noreply.github.com>
2019-06-19 01:03:48 +0900

    messageprocess 거부

--- Message/messageprocess.php
+++ Message/messageprocess.php
@@ -6,20 +6,21 @@ $base->link = '../style.css';
 $db = new DBC;
 $db->DBI();
 if(!isset($_GET['num'])){
-
     echo "<script>alert('잘못된 경로');location.replace('./login.php');</script>";
-
 }
 $num = $_GET['num'];
 $accept = $_GET['accept'];
+echo $accept;
+echo $num;
 
 if ($accept == 0) { // 거절한 경우
-    $db->query = "SELECT Receiver, Sender, Title FROM MESSAGE WHERE Mnumber = ".$num."";
+    $db->query = "SELECT Receiver, Sender, Title FROM MESSAGE WHERE Mnumber = ".$num;
     $db->DBQ();
     $data = $db->result->fetch_row();
-    $db->query = "UPDATE MESSAGE 
-        SET Receiver = '".$data[0]."', Sender = '".$data[1]."', Title = '<거절> '.'".$data[2]."', Mtime = NOW()
-        WHERE Mnumber = ".$num."";
+    $db->query = "UPDATE MESSAGE
+        SET Receiver = '".$data[0]."', Sender = '".$data[1]."', Title = '<거절>".$data[2]."', Mtime = NOW()
+        WHERE Mnumber = ".$num;
+    $db->DBQ();
     echo "<script>alert('거절하였습니다.');history.back();</script>";
 }
 
@@ -41,7 +42,7 @@ elseif ($accept == 1) { // 수락한 경우
     $db->query = "SELECT Receiver, Sender, Title FROM MESSAGE WHERE Mnumber = ".$num."";
     $db->DBQ();
     $data = $db->result->fetch_row();
-    $db->query = "UPDATE MESSAGE 
+    $db->query = "UPDATE MESSAGE
         SET Receiver = '".$data[0]."', Sender = '".$data[1]."', Title = '<수락> '.'".$data[2]."', Mtime = NOW()
         WHERE Mnumber = ".$num."";
     echo "<script>alert('수락하였습니다.');history.back();</script>";
