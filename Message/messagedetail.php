3aec5e253b7fa3cb8c571659fa072db74b66ecd8
meaning2415 <33250675+meaning2415@users.noreply.github.com>
2019-06-19 13:58:46 +0900

    메시지 양식

--- Message/messagedetail.php
+++ Message/messagedetail.php
@@ -13,7 +13,7 @@ if(!isset($_GET['num'])){
 }
 $num = $_GET['num'];
 $link='./messageaprocess.php';
-
+  $base->style = ' th, td{ border-right:1px solid #808080; } h1 {text-align : center;}';
 $db = new DBC;
 
 $db->DBI();
@@ -29,27 +29,64 @@ if($db->result){
   $db->DBQ();
   if($db->result){
     $sdata = $db->result->fetch_row();
+
+    $base->content .= "<table id = 'maintable'>";
     $base->content .= "
-      제목 : ".$data[5]." <br/>
-      보낸 사람 : ".$data[3]." <br/>
-      강의번호 : ".$sdata[1]." <br/>
-      건물번호 : ".$sdata[0]." <br/>
-      강의실 : ".$sdata[2]." <br/>
-      요일 : ".$data[8]." <br/>
-      시간 : ".$data[6]."~".$data[7]." <br/>
-      내용 : ".$data[9]." <br/>
-      발신 시각 : ".$data[10]." <br/>
-      <form action='./messageprocess.php'>
+    <tr style=' border : 1px solid #808080; text-align: center; padding: 10px; ' id=course_text>
+      <td>제목</td>
+      <td style='width:400px;'>".$data[5]."</td>
+    </tr>
+    <tr style=' border : 1px solid #808080; text-align: center; padding: 10px; ' id=course_text>
+      <td>보낸 사람</td>
+      <td>".$data[3]."</td>
+    </tr>
+    <tr style=' border : 1px solid #808080; text-align: center; padding: 10px; ' id=course_text>
+      <td>발신 시각</td>
+      <td>".$data[10]."</td>
+    </tr>
+    <tr style=' border : 1px solid #808080; text-align: center; padding: 10px; ' id=course_text>
+      <td>강의번호</td>
+      <td>".$sdata[1]."</td>
+    </tr>
+    <tr style=' border : 1px solid #808080; text-align: center; padding: 10px; ' id=course_text>
+      <td>건물번호</td>
+      <td>".$sdata[0]."</td>
+    </tr>
+    <tr style=' border : 1px solid #808080; text-align: center; padding: 10px; ' id=course_text>
+      <td>강의실</td>
+      <td>".$sdata[2]."</td>
+    </tr>
+    <tr style=' border : 1px solid #808080; text-align: center; padding: 10px; ' id=course_text>
+      <td>요일</td>
+      <td>".$data[8]."</td>
+    </tr>
+    <tr style=' border : 1px solid #808080; text-align: center; padding: 10px; ' id=course_text>
+      <td>시간</td>
+      <td>".$data[6]."~".$data[7]."</td>
+    </tr>
+    <tr style=' border : 1px solid #808080; text-align: center; padding: 10px; ' id=course_text>
+      <td>내용</td>
+      <td style='height:200px'>".$data[9]."</td>
+    </tr>
+
+    </table>
+    <br>
+    <table style='border-collapse : collapse; table-layout:fixed; margin:auto;'>
+      <td style='border-right:none;'>
+    <form action='./messageprocess.php'>
         <input type = hidden id = 'accept' name = 'accept' value = 1> </input>
         <input type = hidden id = 'num' name = 'num' value = '".$num."'> </input>
         <input type='submit' value='수락'>
-      </form>
-      <form action='./messageprocess.php'>
+        </td>
+        <td style='border-right:none;'>
+      </form><form action='./messageprocess.php'>
         <input type = hidden id = 'accept' name = 'accept' value = 0> </input>
         <input type = hidden id = 'num' name = 'num' value = '".$num."'> </input>
         <input type='submit' value='거부'>
       </form>
-      ";
+      </td>
+      </table>
+    ";
   }
 }
 
